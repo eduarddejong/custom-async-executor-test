3cddfb93eae3f5f167966075c3976c3c1590435a
Eduard de Jong <86660027+eduarddejong@users.noreply.github.com>
2023-12-30 04:13:43 +0100

    Remove unnecessary Mutex and mutating code around Receiver

--- src/custom_async_executor.rs
+++ src/custom_async_executor.rs
@@ -5,37 +5,36 @@ use std::{
     task::Poll,
 };
 
-use crossbeam_channel::Sender;
+use crossbeam_channel::{Receiver, Sender};
 use futures::task::{self, ArcWake};
 
 struct SimpleExecutorArcWake<'a, T> {
     future: Mutex<Pin<&'a mut (dyn Future<Output = T> + Sync + Send)>>,
-    sender: Mutex<Option<Sender<()>>>,
+    sender: Sender<()>,
+    receiver: Receiver<()>,
 }
 
 impl<'a, T> SimpleExecutorArcWake<'a, T> {
-    pub fn execute(self: &Arc<Self>) -> T {
-        let (sender, receiver) = crossbeam_channel::bounded(1);
-        sender.send(()).unwrap();
-        *self.sender.lock().unwrap() = Some(sender);
-        while receiver.recv().is_ok() {
+    fn execute(self: &Arc<Self>) -> T {
+        self.sender.send(()).unwrap();
+        while self.receiver.recv().is_ok() {
             let waker = task::waker_ref(self);
             let context = &mut task::Context::from_waker(&waker);
             let poll = self.future.lock().unwrap().as_mut().poll(context);
             if let Poll::Ready(result) = poll {
-                *self.sender.lock().unwrap() = None;
                 return result;
             }
         }
-        unreachable!("Channel is closed, poll should be ready");
+        unreachable!("Poll should be ready, result should be returned");
     }
 }
 
 impl<T> ArcWake for SimpleExecutorArcWake<'_, T> {
     fn wake_by_ref(arc_self: &Arc<Self>) {
-        if let Some(sender) = arc_self.sender.lock().unwrap().as_ref() {
-            sender.send(()).expect("Send through channel failed");
-        }
+        arc_self
+            .sender
+            .send(())
+            .expect("Send through channel failed");
     }
 }
 
@@ -48,9 +47,11 @@ impl SimpleExecutor {
 
     pub fn block_on<T>(&mut self, mut future: (impl Future<Output = T> + Sync + Send)) -> T {
         let future = pin::pin!(future);
+        let (sender, receiver) = crossbeam_channel::bounded(1);
         Arc::new(SimpleExecutorArcWake {
             future: Mutex::new(future),
-            sender: Mutex::new(None),
+            sender,
+            receiver,
         })
         .execute()
     }
